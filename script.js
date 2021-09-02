323c531cc5ae8f7e337c6a02a0f320e2ffaf46e5
PasichnaDaryna <63604915+PasichnaDaryna@users.noreply.github.com>
2021-09-02 19:19:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,69 @@
-function fizzBuzz(n) {
+// function fizzBuzz(n) {
 
 
-    for (i = 1; i <= n; i++) {
-        // is the number a multiple of 3 and 5
+//     for (i = 1; i <= n; i++) {
+//         // is the number a multiple of 3 and 5
 
-        if (i % 15 === 0) {
-            console.log("fizzbuzz")
-        } else
-            if (i % 3 === 0) {
-                console.log("fizz")
-            }
-            else
-                if (i % 5 === 0) {
-                    console.log("buzz")
-                }
-                else {
-                    console.log(i)
-                }
-
-
-    }
+//         if (i % 15 === 0) {
+//             console.log("fizzbuzz")
+//         } else
+//             if (i % 3 === 0) {
+//                 console.log("fizz")
+//             }
+//             else
+//                 if (i % 5 === 0) {
+//                     console.log("buzz")
+//                 }
+//                 else {
+//                     console.log(i)
+//                 }
 
 
+//     }
 
-}
 
 
-fizzBuzz(5)
+// }
+
+
+// function chunk(array, size) {
+
+//     let chunked = [];
+
+//     for (let element of array) {
+//         const last = chunked[chunked.length - 1];
+
+//         if (!last || last.length === size) {
+//             chunked.push([element]);
+
+//         } else {
+//             last.push(element);
+//         }
+//     }
+//     return chunked;
+
+
+// }
+
+
+// 
+//steps
+// function steps(n) {
+//     for (let row = 0; row < n; row++) {
+//         let stair = '';
+
+
+//         for (let column = 0; column < n; column++) {
+//             if (column <= row) {
+//                 stair += '#'
+//             } else {
+//                 stair += ' ';
+//             }
+//         }
+//         console.log(stair)
+//     }
+// }
+
+
+// steps(3)
+//  
