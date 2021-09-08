ebc2e47d57e454dc843d4634822a1fd16f230aac
PasichnaDaryna <63604915+PasichnaDaryna@users.noreply.github.com>
2021-09-08 09:53:57 +0200

    Update closures.js

--- closures.js
+++ closures.js
@@ -1,3 +1,6 @@
+// Это способность функции во время создания запоминать ссылки на переменные функции и параметры, находящиеся в текущем лексическом окружении, а так же в лексическом окружении родительской функции и так до глобальной области видимости. Замыкание подразумивает именно внешние переменные, а не саму функцию.
+
+
 // const runOnce = function () {
 //     console.log('this will never run again')
 // }
@@ -70,3 +73,49 @@
 
 // var calc = generator(900)
 // console.log(calc())
+
+
+
+// function createCulcFunction(n) {
+
+
+// return function () {
+//     console.log(1000 * n)
+// }
+// }
+
+// createCulcFunction(42)
+// // nothing will return because it returns another function
+
+// const calc = createCulcFunction(42)
+// calc()
+
+// function createIncrementor(n) {
+//     return function (num) {
+//         return n + num;
+
+//     }
+// }
+// const addOne = createIncrementor(1)
+// const addTen = createIncrementor(10)
+
+// console.log(addOne(10))
+// console.log(addOne(41))
+
+
+
+// console.log(addTen(10))
+// console.log(addTen(41))
+
+
+
+
+function urlGenerator(domain) {
+    return function (url) {
+        return `https://${url}.${domain}`
+    }
+}
+
+
+const comUrl = urlGenerator('com')
+console.log(comUrl('google'))
